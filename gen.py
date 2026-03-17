79c9aa4448b39691cf0cc794fb4a6543cef519c7
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-03-17 08:28:52 -0400

    add cutoff from the wall

--- gen.py
+++ gen.py
@@ -6,6 +6,10 @@ random.seed(122345)
 
 def good(x, y):
     for i in range(n):
+        if x[i] < cutoff or x[i] > 1 - cutoff:
+            return False
+        if y[i] < cutoff or y[i] > 1 - cutoff:
+            return False
         for j in range(i + 1, n):
             if math.hypot(x[i] - x[j], y[i] - y[j]) < cutoff:
                 return False
@@ -17,7 +21,7 @@ cnt = 0
 while cnt < n:
     x = [random.uniform(0, 1) for _ in range(n)]
     y = [random.uniform(0, 1) for _ in range(n)]
-    om = [random.uniform(-1, 1) for _ in range(n)]
+    om = [random.choice([-1, 1]) * random.uniform(0.5, 1) for _ in range(n)]
     if good(x, y):
         d = f"{cnt:08d}"
         os.makedirs(d, exist_ok=True)
