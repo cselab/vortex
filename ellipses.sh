de9a5d1835098d5ec13bc7f381adbb6edfb26cff
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-03-22 13:17:52 -0400

    up

--- ellipses.sh
+++ ellipses.sh
@@ -1,7 +1,15 @@
+#!/bin/sh
+
+: ${QCC=qcc}
+if ! command >/dev/null -v "$QCC"
+then printf 'ellipses.sh: error: '\''%s'\'' is not in PATH\n' "$QCC"
+     exit 1
+fi
+
 a=0.200
 b=0.025
 c=0.010
-qcc -disable-dimensions ellipses.c -O2 -lm -o ellipses &&
+"$QCC" -disable-dimensions ellipses.c -O2 -lm -o ellipses &&
 ./ellipses \
    0.5 0.7 1 $a $b 0 \
    0.5 0.3 1 $a $b 0 \
